32fd13efbece7caa13d86725be41539f74cf3b42
trieu.ngoquang <trieu.ngoquang@vti.com.vn>
2020-10-13 15:35:38 +0700

    config .env

--- web-app-restful/db.js
+++ web-app-restful/db.js
@@ -1,19 +1,11 @@
 'use strict';
 const mysql = require('mysql');
-
+require('dotenv').config()
 const db = mysql.createConnection({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME_DATABASE,
-    host: 'localhost',
-    user: 'root',
-    password: 'bemyself',
-    database: 'nodejs_api',
-    insecureAuth : true
-}).connect((err) => {
-    if(err) throw err;
-    console.log('Connected!')
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_DATABASE,
 });
 
 
