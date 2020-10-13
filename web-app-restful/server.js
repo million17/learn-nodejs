84bd2470a4eec924c44050066a0aed1b7b078714
trieu.ngoquang <trieu.ngoquang@vti.com.vn>
2020-10-13 10:43:46 +0700

    config server

--- web-app-restful/server.js
+++ web-app-restful/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server started : ${port}`);
+});
 
 app.get('/', (req,res) => {
     res.send('Hello Express');
