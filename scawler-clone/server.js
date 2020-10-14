f4fa79fd68965d1cfdd2316759dce7a7601f1323
Trieu Ngo Quang <ngoquangtrieu99tb@gmail.com>
2020-10-14 22:16:52 +0700

    push code

--- scawler-clone/server.js
+++ scawler-clone/server.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 //Require dependencies
+const path = require('path')
 const logger = require('morgan');
 const express = require('express');
 
+const {sendResponse} = require('./app/helpers');
+const {fetchAuthorProfile} = require('./app/scotch')
 //Create an Express Application
 const app = express();
+// app.use('/', express.static(path.resolve(__dirname), '/public'));
+app.use(express.static(__dirname + '/public'));
 
+app.get('/:author', (req, res, next) => {
+    const auth = req.params.author;
+    sendResponse(res)(fetchAuthorProfile(auth));
+})
 // Configure the app port
 const port = process.env.HOST || 9999;
 
